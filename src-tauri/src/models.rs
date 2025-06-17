af8f2dcac0b2dbee458dc00695cf051dda319325
LeBoufty <boufto@gmail.com>
2025-06-17 22:06:18 +0200

    Responses

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+// Template models
+// Used to construct the form in the frontend
+
 #[derive(Serialize, Deserialize)]
 /** The rule by which the score is calculated */
 pub enum ScoringRule {
@@ -35,4 +38,53 @@ pub struct Check {
     pub ctype: CheckType,
     /** Check weight */
     pub weight: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+/** A category in a checklist */
+pub struct ChecklistCategory {
+    /** Category name */
+    pub name: String,
+    /** Checks in the category */
+    pub checks: Vec<Check>,
+}
+
+#[derive(Serialize, Deserialize)]
+/** A checklist template, which is used to rank entries */
+pub struct Checklist {
+    /** Checklist ID */
+    pub id: usize,
+    /** Checklist name */
+    pub name: String,
+    /** Categories in the checklist */
+    pub categories: Vec<ChecklistCategory>,
+}
+
+// Response models
+// Used for score computations
+
+#[derive(Serialize, Deserialize)]
+pub struct CheckResponse {
+    /** The check that is being responded to */
+    pub check: Check,
+    /** The value given by the user */
+    pub value: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CategoryResponse {
+    /** Name of the category, same as in template */
+    pub name: String,
+    /** Individual checks' responses */
+    pub checks: Vec<CheckResponse>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChecklistResponse {
+    /** Template ID */
+    pub template_id: usize,
+    /** Response name, different from checklist name */
+    pub name: String,
+    /** Categories in the response */
+    pub categories: Vec<CategoryResponse>,
 }
--- src-tauri/src/scoring.rs
+++ src-tauri/src/scoring.rs
@@ -1,9 +1,9 @@
-use crate::models::ScoringRule;
+use crate::models::{CategoryResponse, CheckResponse, CheckType, ChecklistResponse, ScoringRule};
 
 impl ScoringRule {
     /** Calculates the score for a given value following the rule. */
     // Check src/models.rs for specifics on each rule.
-    pub fn compute_score(self, value: f32) -> f32 {
+    pub fn compute_score(&self, value: &f32) -> f32 {
         match self {
             Self::ZeroToOne(copt) => {
                 if value > copt {
@@ -48,5 +48,35 @@ impl ScoringRule {
                 }
             }
         }
+    }
+}
+
+impl CheckResponse {
+    pub fn get_score(&self) -> f32 {
+        match &self.check.ctype {
+            CheckType::Checkbox => self.value,
+            CheckType::Stars => self.value,
+            CheckType::Value(rule) => rule.compute_score(&self.value),
+        }
+    }
+}
+
+impl CategoryResponse {
+    pub fn get_total_weight(&self) -> f32 {
+        self.checks.iter().map(|r| r.check.weight).sum()
+    }
+
+    pub fn get_score(&self) -> f32 {
+        let tweight = self.get_total_weight();
+        self.checks
+            .iter()
+            .map(|r| r.get_score() * r.check.weight / tweight)
+            .sum()
+    }
+}
+
+impl ChecklistResponse {
+    pub fn get_score(self) -> f32 {
+        self.categories.iter().map(|c| c.get_score()).sum::<f32>() / (self.categories.len() as f32)
     }
 }
