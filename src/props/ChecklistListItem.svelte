e64f3eeb6f7f45fb1df9ee0de533bc25f85dd416
LeBoufty <boufto@gmail.com>
2025-06-19 13:56:25 +0200

    Changed logo in app

--- src/props/ChecklistListItem.svelte
+++ src/props/ChecklistListItem.svelte
@@ -6,7 +6,7 @@
   <div
     class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-zinc-900 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
   >
-    <img class="size-12 shrink-0" src="/tauri.svg" alt="Tauri Logo" />
+    <img class="size-12 shrink-0" src="/logo.png" alt="Sift Logo" />
     <div class="max-w-full text-ellipsis overflow-hidden">
       <div class="text-xl font-medium text-black dark:text-white">
         {cl.name}
